7e785e044e43746ea075c9a181f40fef740ea508
Programador Novato <info@programadornovato.com>
2021-01-16 15:41:38 -0600

    POO Método Constructor en Android Studio [57]
    Video:
    https://youtu.be/JyvUBf-SKmE

--- MainActivity.kt
+++ MainActivity.kt
@@ -7,16 +7,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var cal=Calculadora()
-        var n1=1
-        var n2=2
-        var res=cal.suma(n1,n2)
-        println("Humano aqui esta tu pinche suma de $n1 y $n2 = $res")
-        res=cal.resta(n1,n2)
-        println("Humano aqui esta tu pinche resta de $n1 y $n2 = $res")
-        res=cal.multiplicacion(n1,n2)
-        println("Humano aqui esta tu pinche multiplicacion de $n1 y $n2 = $res")
-        var resD=cal.divicion(n1.toDouble(),n2.toDouble())
-        println("Humano aqui esta tu pinche divicion de $n1 y $n2 = $resD")
+        var p1=Persona("Juan",1)
+        //p1.inicializar("Juan",1)
+        println("Nombre=${p1.nombre} edad=${p1.edad}")
     }
 }
---
+++ Persona.kt
@@ -0,0 +1,18 @@
+package com.programadornovato.myapplication
+
+class Persona(_nombre:String,_edad:Int){
+    var nombre=_nombre
+    var edad=_edad
+    /*
+    fun inicializar(_nombre:String,_edad:Int){
+        nombre=_nombre
+        edad=_edad
+    }
+    */
+    /*
+    constructor(_nombre:String,_edad:Int){
+        nombre=_nombre
+        edad=_edad
+    }
+    */
+}
