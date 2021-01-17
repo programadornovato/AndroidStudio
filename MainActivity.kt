9af8f329d9dd70a5d18bd57b1a3379aa2fdcafc0
Programador Novato <info@programadornovato.com>
2021-01-17 01:32:39 -0600

    POO Crear paquetes y clases en Android Studio [59]
    Video:
    https://youtu.be/zEhKEKP3jLI

--- MainActivity.kt
+++ MainActivity.kt
@@ -2,15 +2,16 @@ package com.programadornovato.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.programadornovato.myapplication.Matematicas.Basicas
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var v1=Vehiculo("Vocho","1980")
-        //println("marca=${v1.marca} modelo=${v1.modelo}")
-        v1.acelerar()
-        var v2=Vehiculo("654654")
-        v2.acelerar(6)
+        var matBasicas=Basicas(1,2)
+        println("Suma=${matBasicas.suma()}")
+        println("Resta=${matBasicas.resta()}")
+        println("Multiplicacion=${matBasicas.multiplicacion()}")
+        println("Divicion=${matBasicas.divicion()}")
     }
 }
---
+++ Matematicas/Avanzadas.kt
@@ -0,0 +1,2 @@
+package com.programadornovato.myapplication.Matematicas
+
